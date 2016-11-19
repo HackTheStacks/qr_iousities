7e6f4f130fb2efca4e29f986a31edf25114d11d9
Katerina Iliakopoulou <katerina.iliakopoulou@gmail.com>
2016-11-19 17:38:39 -0500

    basic api methods

--- server/app.py
+++ server/app.py
@@ -1,6 +1,29 @@
+#!/usr/bin/env python
+
+import sys
+import logging
+import json
+
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/get_artifact", methods=["GET", "POST", "OPTIONS"])
+def get_artifact():
+    return 'Give me my artifact!'
+
+@app.route("/s/<short_url>", methods=["GET", "POST", "OPTIONS"])
+def redirect(short_url):
+    return 'Redirect me to: ' + short_url
+
+@app.route("/update_artifact", methods=["GET", "POST", "OPTIONS"])
+def update_artifact():
+    return 'Update my artifact!'
+
+
+if __name__ == '__main__':
+	description = """QR_iosities API"""
+
+	logging.basicConfig(level=logging.INFO)
+	logger = logging.getLogger("QR_iosities")
+
+	app.run(host='0.0.0.0', port=8000)
