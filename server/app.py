df051bd95c6af70feb5f42b172cd60ca8c059449
Katerina Iliakopoulou <katerina.iliakopoulou@gmail.com>
2016-11-19 17:46:27 -0500

    comments on sqLite queries

--- server/app.py
+++ server/app.py
@@ -3,12 +3,16 @@
 import sys
 import logging
 import json
+import sqlite3
 
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/get_artifact", methods=["GET", "POST", "OPTIONS"])
 def get_artifact():
+	"""
+	DB QUERY: SELECT ALL NECESSARY FIELDS TO DISPLAY ON UI BASED ON LONG URL (assume you have that)
+	"""
     return 'Give me my artifact!'
 
 @app.route("/s/<short_url>", methods=["GET", "POST", "OPTIONS"])
@@ -17,6 +21,9 @@ def redirect(short_url):
 
 @app.route("/update_artifact", methods=["GET", "POST", "OPTIONS"])
 def update_artifact():
+	"""
+	DB QUERY: UPDATE LONG URL BASED ON SHORT URL (assume you have that)
+	"""
     return 'Update my artifact!'
 
 
