f7ee14282218e2dcde89d22420b3e1b0f512a090
angelaguo <angela.guo@nytimes.com>
2016-11-19 22:11:09 -0500

    add database connections

--- server/app.py
+++ server/app.py
@@ -9,21 +9,43 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/get_artifact", methods=["GET", "POST", "OPTIONS"])
-def get_artifact():
+def get_artifact(long_url):
 	"""
 	DB QUERY: SELECT ALL NECESSARY FIELDS TO DISPLAY ON UI BASED ON LONG URL (assume you have that)
+    longUrl is of type string
 	"""
-    return 'Give me my artifact!'
+    # returns everything that UI needs (basically everything but table ID)
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    url = (long_url,)
+    c.execute('SELECT * FROM items WHERE LongUrl=?' , url)
+    response = c.fetchone()
+
+    #if there is no entry with this longUrl, insert a new entry
+    # itemId, name, descriptor, shortUrl, longUrl aren't defined here at the moment but will need to be supplied by the user
+    if response is None:
+        c.execute('INSERT INTO items VALUES (?,?,?,?,?)', (itemId, name, descriptor, shortUrl, longUrl))
+        conn.commit()
+    else:
+        return response
+    conn.close()
 
 @app.route("/s/<short_url>", methods=["GET", "POST", "OPTIONS"])
 def redirect(short_url):
     return 'Redirect me to: ' + short_url
 
 @app.route("/update_artifact", methods=["GET", "POST", "OPTIONS"])
-def update_artifact():
+def update_artifact(short_url, long_url):
 	"""
 	DB QUERY: UPDATE LONG URL BASED ON SHORT URL (assume you have that)
 	"""
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    s_url = (short_url,)
+    l_url = (long_url,)
+    c.execute('UPDATE items SET LongUrl = ? WHERE ShortUrl = ?', (l_url, s_url) )
+    conn.commit()
+    conn.close()
     return 'Update my artifact!'
 
 
