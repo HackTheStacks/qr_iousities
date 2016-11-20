9899ba79080d51be6a65ec3c8efc8b9e6cf87dfb
Katerina Iliakopoulou <katerina.iliakopoulou@gmail.com>
2016-11-19 23:34:29 -0500

    update api with latest changes

--- server/app.py
+++ server/app.py
@@ -10,23 +10,23 @@ app = Flask(__name__)
 
 @app.route("/get_artifact", methods=["GET", "POST", "OPTIONS"])
 def get_artifact():
-	"""
-	DB QUERY: SELECT ALL NECESSARY FIELDS TO DISPLAY ON UI BASED ON LONG URL (assume you have that)
-	"""
-	data = request.form
-	long_url = None
-	if 'longUrl' in data:
-		long_url = data['longUrl']
-		
-	res = json.dumps(data)
+    """
+    DB QUERY: SELECT ALL NECESSARY FIELDS TO DISPLAY ON UI BASED ON LONG URL (assume you have that)
+    """
+    data = request.form
+    long_url = None
+    if 'longUrl' in data:
+        long_url = data['longUrl']
+        
+    res = json.dumps(data)
     return res
 
 @app.route("/get_all_artifacts", methods=["GET", "OPTIONS"])
 def get_all_artifacts():
-	conn = sqlite3.connect('database.db')
-	c = conn.cursor()
-	c.execute('SELECT * FROM items')
-	response = c.fetchone()
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    c.execute('SELECT * FROM items')
+    response = c.fetchone()
     return json.dumps(response)
 
 @app.route("/s/<short_url>", methods=["GET", "POST", "OPTIONS"])
@@ -35,19 +35,19 @@ def redirect(short_url):
 
 @app.route("/update_artifact", methods=["GET", "POST", "OPTIONS"])
 def update_artifact():
-	"""
-	DB QUERY: UPDATE LONG URL BASED ON SHORT URL (assume you have that)
-	"""
+    """
+    DB QUERY: UPDATE LONG URL BASED ON SHORT URL (assume you have that)
+    """
     data = request.form
     if 'longUrl' in data:
-		long_url = data['longUrl']
-		short_url = data['shortUrl']
+        long_url = data['longUrl']
+        short_url = data['shortUrl']
     return data
 
 if __name__ == '__main__':
-	description = """QR_iosities API"""
+    description = """QR_iosities API"""
 
-	logging.basicConfig(level=logging.INFO)
-	logger = logging.getLogger("QR_iosities")
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger("QR_iosities")
 
-	app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
