f7b6026ff250c02d3eab8a3c3ac247f4e87093a2
chl557 <chl557@nyu.edu>
2016-11-20 03:49:22 -0500

    create qr table

--- server/app.py
+++ server/app.py
@@ -11,6 +11,7 @@ import sqlite3
 import os
 import cStringIO
 import base64
+import add_qrtable
 
 from flask import Flask, redirect, request, Response
 from flask import send_file
@@ -109,7 +110,7 @@ def gen_qr_code(url):
     qr.add_data(url)
     qr.make(fit=True)
     img = qr.make_image()
-    
+
     return img
 
 @app.route('/get_qrimg/<url>')
@@ -121,16 +122,33 @@ def get_qrimg(url):
     output: img_url which represents the qrcode
     """
     img_buf = cStringIO.StringIO()
-    img = gen_qr_code(url)
-    img.save(img_buf)
-    im_data = img_buf.getvalue()
-    data_url = 'data:image/svg+xml;base64,' + base64.encodestring(im_data)
-    content = json.dumps(data_url.strip())
+    conn = sqlite3.connect('database.db')
+
+    add_qrtable.create_table(conn)
+    c = conn.cursor()
+    c.execute("SELECT EXISTS(SELECT qrcode_base FROM qrcode WHERE shortUrl = ?)",(url,))
+    check_data=c.fetchone()
+    print check_data
+    if check_data[0] == 0:
+        print "i dont have value"
+        img = gen_qr_code(url)
+        img.save(img_buf)
+        im_data = img_buf.getvalue()
+        data_url = 'data:image/svg+xml;base64,' + base64.encodestring(im_data)
+        content = json.dumps(data_url.strip())
+        c.execute("INSERT OR IGNORE INTO qrcode(shortUrl, qrcode_base) VALUES(?, ?)",(url,content))
+        conn.commit()
+    else:
+        content = check_data[0]
     resp = Response(content, mimetype='application/json')
     resp.headers['Access-Control-Allow-Origin'] = '*'
     resp.headers['Access-Control-Allow-Method'] = 'GET, OPTIONS'
     resp.headers['Access-Control-Allow-Headers'] = 'Content-Type'
     return resp
+
+
+
+
 
 
 if __name__ == '__main__':
