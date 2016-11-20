38f5d26550e9fba3db506af78499dd18cf55ccf6
Nicole <nicole.baram@nytimes.com>
2016-11-19 23:52:36 -0500

    added dropdown

--- client/src/components/Create/index.js
+++ client/src/components/Create/index.js
@@ -9,11 +9,13 @@ class Create extends React.Component {
 
     this.state = {
       artifactUrl: 'something',
-      artifact: null
+      artifact: null,
+      type: ''
     };
 
     this.handleOnSubmit = this.handleOnSubmit.bind(this);
     this.handleOnChange = this.handleOnChange.bind(this);
+    this.handleClickDropdown = this.handleClickDropdown.bind(this);
   }
 
   handleOnChange(event) {
@@ -22,21 +24,27 @@ class Create extends React.Component {
     });
   }
 
+  handleClickDropdown(event) {
+    this.setState({
+      type: event.target.value
+    })
+  }
+
   handleOnSubmit(event) {
     event.preventDefault();
 
-    axios.post('/get_artifact', {
-      longUrl: this.state.artifactUrl
-    })
-    .then((data) => {
-      console.log(data);
-      this.setState({
-        artifact: data
-      })
-    })
-    .catch((err) => {
-      console.log(err);
-    })
+    // axios.post('/get_artifact', {
+    //   longUrl: this.state.artifactUrl
+    // })
+    // .then((data) => {
+    //   console.log(data);
+    //   this.setState({
+    //     artifact: data
+    //   })
+    // })
+    // .catch((err) => {
+    //   console.log(err);
+    // })
   };
 
 
@@ -53,6 +61,9 @@ class Create extends React.Component {
           <h1>Search</h1>
           <form onSubmit={this.handleOnSubmit}>
             <div>Please input the artifact you would like to find as a url.</div>
+            <select onChange={this.handleClickDropdown} value={this.state.type}>
+              <option value="BHL">BHL</option>
+            </select>
             <input type="text" value={this.state.artifactUrl} onChange={this.handleOnChange} className="searchBar" />
             <input type="submit" value="Search"/>
           </form>
