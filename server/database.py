ba07bf513328e84f018f5b4cfeb958a79f33aeef
Tony Li <tonyli.lives@gmail.com>
2016-11-20 03:01:00 -0500

    add database abstraction

--- server/database.py
+++ server/database.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python
 
 import sqlite3
+from flask import g
 
+DATABASE = 'database.db'
 
 class DB():
-	
+    def __init__(self):
+        self.db = sqlite3.connect(DATABASE)
+
+    def get_db():
+        db = getattr(g, '_database', None)
+        if db is None:
+            db = g._database = sqlite3.connect(DATABASE)
+        return db
+
+    def close_connection(exception):
+        db = getattr(g, '_database', None)
+        if db is not None:
+            db.close()
+
+    def query(self, query, args=(), one=False):
+        cur = self.db.execute(query, args)
+        results = cur.fetchall()
+        cur.close()
+        return (results[0] if results else None) if one else results
