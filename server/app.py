d717ee4c4411aaeb3b66a507ec77bfcba6514b16
Katerina Iliakopoulou <katerina.iliakopoulou@gmail.com>
2016-11-20 00:08:37 -0500

    add cors

--- server/app.py
+++ server/app.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python
 
+import sys
 import logging
 import json
 import sqlite3
-import os
 
-from flask import Flask ,redirect, request
+from flask import Flask, redirect, request, Response
 app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return 'Redirect to front-end!'
 
 @app.route("/get_artifact", methods=["GET", "POST", "OPTIONS"])
 def get_artifact():
@@ -21,21 +17,35 @@ def get_artifact():
     long_url = None
     if 'longUrl' in data:
         long_url = data['longUrl']
+        
+    content = json.dumps(data)
 
-    res = json.dumps(data)
-    return res
+    resp = Response(content, mimetype='application/json')
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Access-Control-Allow-Method'] = 'GET, OPTIONS'
+    resp.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    
+    return resp
 
 @app.route("/get_all_artifacts", methods=["GET", "OPTIONS"])
 def get_all_artifacts():
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect('../testDatabase.db')
     c = conn.cursor()
     c.execute('SELECT * FROM items')
     response = c.fetchone()
-    return json.dumps(response)
+    
+    content = json.dumps(response)
+
+    resp = Response(content, mimetype='application/json')
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Access-Control-Allow-Method'] = 'GET, OPTIONS'
+    resp.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    
+    return resp
 
 @app.route("/s/<short_url>", methods=["GET", "POST", "OPTIONS"])
-def redirect(short_url):
-    return redirect(short_url, code=302)
+def redirect_url(short_url):
+    return redirect("http://www.cnn.com/")
 
 @app.route("/update_artifact", methods=["GET", "POST", "OPTIONS"])
 def update_artifact():
@@ -46,26 +56,20 @@ def update_artifact():
     if 'longUrl' in data:
         long_url = data['longUrl']
         short_url = data['shortUrl']
-    res = json.dumps(data)
-    return res
+    
+    content = json.dumps(data)
 
-def get_level(level):
-    if level == 'CRITICAL':
-        return logging.CRITICAL
-    elif level == 'ERROR':
-        return logging.ERROR
-    elif level == 'WARNING':
-        return logging.WARNING
-    elif level == 'DEBUG':
-        return logging.DEBUG
-    else:
-        return logging.INFO
+    resp = Response(content, mimetype='application/json')
+    resp.headers['Access-Control-Allow-Origin'] = '*'
+    resp.headers['Access-Control-Allow-Method'] = 'GET, OPTIONS'
+    resp.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    
+    return resp
 
 if __name__ == '__main__':
     description = """QR_iosities API"""
 
-    level = get_level(os.environ.get('LOG_LEVEL', 'INFO'))
-    logging.setLevel(level)
+    logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger("QR_iosities")
 
     app.run(host='0.0.0.0')
