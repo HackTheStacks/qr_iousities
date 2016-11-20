9583064ffa719ebd3c4e7b9c2a84594c8a9e7d9b
Adrian Fraiha <aef@fastmail.com>
2016-11-20 01:40:41 -0500

    Styles for create + error messages

--- client/src/components/Create/index.js
+++ client/src/components/Create/index.js
@@ -2,7 +2,17 @@
 import React from 'react';
 import axios from 'axios';
 import config from '../../config';
-// import styles from './styles.scss';
+import styles from './styles.scss';
+
+const FORMATS_BY_TYPE = {
+    BHL: {
+        regex: new RegExp('http://www.biodiversitylibrary.org/page'),
+        examples: [
+            'https://www.biodiversitylibrary.org/item/16800',
+            'https://www.biodiversitylibrary.org/item/16800#page/5/mode/1up'
+        ]
+    }
+};
 
 class Create extends React.Component {
   constructor(props) {
@@ -11,7 +21,8 @@ class Create extends React.Component {
     this.state = {
       artifactUrl: 'something',
       artifact: null,
-      type: ''
+      type: 'BHL',
+      status: null
     };
 
     this.handleOnSubmit = this.handleOnSubmit.bind(this);
@@ -33,6 +44,14 @@ class Create extends React.Component {
 
   handleOnSubmit(event) {
     event.preventDefault();
+      const format = FORMATS_BY_TYPE[this.state.type];
+      this.setState({status: null});
+
+      if(!format.regex.test(this.state.artifactUrl)) {
+          this.setState({status: 'error'});
+          return;
+      }
+
 
     axios.post(`${config.apiUrl}/get_artifact`, {
       longUrl: this.state.artifactUrl
@@ -40,33 +59,66 @@ class Create extends React.Component {
     .then((data) => {
       console.log(data);
       this.setState({
-        artifact: data
+          artifact: data,
+          status: 'success'
       })
     })
     .catch((err) => {
       console.log(err);
     })
-  };
+  }
 
+  getSuccessMessage() {
+    return (
+        <div className={styles.success}>
+            Saved!
+        </div>
+    );
+  }
+
+  getErrorMessage() {
+      const exampleUrls = FORMATS_BY_TYPE[this.state.type].examples.map((url, i) => {
+          return <li key={i} className={styles.errorItem}>{url}</li>
+      });
+
+    return (
+      <div className={styles.error}>
+        <div className={styles.errorMessage}>URLs must be in the following formats:</div>
+        {exampleUrls}
+      </div>
+    );
+  }
+
+  getInstructionMessage() {
+    return <div className={styles.instructions}>Please input the artifact you would like to find as a url.</div>;
+  }
 
   handleResults() {
-    console.log('results');
     return (
       <div>{this.state.artifactUrl}</div>
     );
   }
 
     render() {
+        let message;
+        switch(this.state.status) {
+          case 'error': message = this.getErrorMessage(); break;
+          case 'success': message = this.getSuccessMessage(); break;
+          default: message = this.getInstructionMessage(); break;
+        }
+
       return (
         <section>
-          <h1>Search</h1>
+          <h1>Create</h1>
+          {message}
           <form onSubmit={this.handleOnSubmit}>
-            <div>Please input the artifact you would like to find as a url.</div>
-            <select onChange={this.handleClickDropdown} value={this.state.type}>
-              <option value="BHL">BHL</option>
-            </select>
-            <input type="text" value={this.state.artifactUrl} onChange={this.handleOnChange} className="searchBar" />
-            <input type="submit" value="Search"/>
+              <div className={styles.searchInputContainer}>
+                <select onChange={this.handleClickDropdown} value={this.state.type}>
+                  <option value="BHL">BHL</option>
+                </select>
+                <input type="text" value={this.state.artifactUrl} onChange={this.handleOnChange} className={styles.searchInput} />
+                <input type="submit" value="Save" className={styles.searchButton}/>
+              </div>
           </form>
           {this.state.artifact ? this.handleResults : null}
         </section>
